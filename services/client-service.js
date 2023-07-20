5bcd08c2849da8f76ce8720d95f9fa5e60722062
Alejandro Flórez Larrea <aflorez68@gmail.com>
2023-07-20 15:27:29 -0500

    Proyecto final

--- services/client-service.js
+++ services/client-service.js
@@ -41,7 +41,7 @@ const actualizarProducto = (url, categoria, productName, price, description, id)
 
 // Leer admins
 
-const listaAdmins = () => fetch("http://localhost:3000/admins").then( respuesta => respuesta.json());
+const listaAdmins = () => fetch("https://github.com/alejoflop/AluraGeekG5/blob/main/db.json").then( respuesta => respuesta.json());
 
 // Ver más
 
